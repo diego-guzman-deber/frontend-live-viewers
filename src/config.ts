98453eb58d5fdff047fbe9171cb9235c531dc5bf
Diego Guzman <diegoguzman366@gmail.com>
2026-05-14 11:42:21 -0400

    update ruta

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@
 export const API_BASE_URL = "https://live-viewers.eldeber.bo";
 
 export const API_ENDPOINTS = {
-  TIKTOK_CACHE: `${API_BASE_URL}/viewers-cache/tiktok`,
-  YOUTUBE_CACHE: `${API_BASE_URL}/viewers-cache/youtube`,
-  FACEBOOK_CACHE: `${API_BASE_URL}/viewers-cache/facebook`,
-  ALL_CACHE_STREAM: `${API_BASE_URL}/viewers-cache/stream`,
-  TIKTOK_LIVE: `${API_BASE_URL}/tiktok/live`,
-  HISTORY_AVERAGES: `${API_BASE_URL}/viewers-history/averages`,
+  TIKTOK_CACHE: `${API_BASE_URL}/api/v1/viewers-cache/tiktok`,
+  YOUTUBE_CACHE: `${API_BASE_URL}/api/v1/viewers-cache/youtube`,
+  FACEBOOK_CACHE: `${API_BASE_URL}/api/v1/viewers-cache/facebook`,
+  ALL_CACHE_STREAM: `${API_BASE_URL}/api/v1/viewers-cache/stream`,
+  TIKTOK_LIVE: `${API_BASE_URL}/api/v1/tiktok/live`,
+  HISTORY_AVERAGES: `${API_BASE_URL}/api/v1/viewers-history/averages`,
 };
